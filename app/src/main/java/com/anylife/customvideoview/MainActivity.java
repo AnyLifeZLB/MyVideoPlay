b21540e1ede849a7797a65bb0ec051a2f434b3ee
B5FCH07.曾刘保 <zenglb@vanke.net.cn>
2016-11-17 11:56:24 +0800

    准备play hls m3u8 live stream

--- app/src/main/java/com/anylife/customvideoview/MainActivity.java
+++ app/src/main/java/com/anylife/customvideoview/MainActivity.java
@@ -3,23 +3,32 @@ package com.anylife.customvideoview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.anylife.customvideoview.VideoView.CustomVideoView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private LiveVideoView liveVideoView;
+    private CustomVideoView customVideoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        liveVideoView=(LiveVideoView)findViewById(R.id.videoView);
-        List<String> path=new ArrayList<>();
-        path.add("http://web-cache.stream.ne.jp/www11/nhkworld-tv/dwstv/222468/live.m3u8");
-        liveVideoView.setVideoPaths(path);
+//        liveVideoView=(LiveVideoView)findViewById(R.id.videoView);
+//        List<String> path=new ArrayList<>();
+//        path.add("http://web-cache.stream.ne.jp/www11/nhkworld-tv/dwstv/222468/live.m3u8");
+//        liveVideoView.setVideoPaths(path);
 
 //        liveVideoView.setVideoPath(path.get(0));
+
+
+        customVideoView=(CustomVideoView)findViewById(R.id.custom_videoView);
+
+        customVideoView.setVideoPath("http://web-cache.stream.ne.jp/www11/nhkworld-tv/dwstv/222468/live.m3u8");
+
 
     }
 }
