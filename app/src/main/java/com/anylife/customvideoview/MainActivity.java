d892e6ea9f2fbf7d7094d080132905498f114084
liubao.zeng <any_life@163.com>
2016-11-19 12:21:18 +0800

    23

--- app/src/main/java/com/anylife/customvideoview/MainActivity.java
+++ app/src/main/java/com/anylife/customvideoview/MainActivity.java
@@ -15,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
 
     private LiveVideoView liveVideoView;
     private CustomVideoView customVideoView;
+    private String path1="http://web-cache.stream.ne.jp/www11/nhkworld-tv/dwstv/222468/live.m3u8";
+    private String path2="http://chyd-wsvod.wasu.tv/data13/ott/344/2015-05/28/1432782476341_377935/playlist.m3u8";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +26,13 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"NHK Playing ...",Toast.LENGTH_LONG).show();
 
 //        VideoView vv = (VideoView)findViewById(R.id.videoView1);
-//        vv.setVideoPath("http://chyd-wsvod.wasu.tv/data13/ott/344/2015-05/28/1432782476341_377935/playlist.m3u8");
-//        //设置媒体控制条
+//        vv.setVideoPath(path2);
+//        //设置媒体控制条`
 //        vv.setMediaController(new MediaController(this));
 //        vv.start();
-
-        //================================================================================================================
-
+//
+//        //================================================================================================================
+//
 //        liveVideoView=(LiveVideoView)findViewById(R.id.videoView);
 //        List<String> path=new ArrayList<>();
 //        path.add("http://chyd-wsvod.wasu.tv/data13/ott/344/2015-05/28/1432782476341_377935/playlist.m3u8");
@@ -38,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         //===================================================================================================================
         customVideoView=(CustomVideoView)findViewById(R.id.custom_videoView);
-        customVideoView.setVideoPath("http://web-cache.stream.ne.jp/www11/nhkworld-tv/dwstv/222468/live.m3u8");
+        customVideoView.setVideoPath(path2);
         customVideoView.start();
 
     }
